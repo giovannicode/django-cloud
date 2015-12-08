462e7e9421640f643af8d1b6e1cd162b0eb5f99a
Giovanni Arroyo <giovannicode7@gmail.com>
2015-12-08 17:38:58 -0600

    add functionality to add song by url

--- cloud/apps/songs/models.py
+++ cloud/apps/songs/models.py
@@ -7,10 +7,37 @@ from apps.albulms.models import Albulm
 import uuid
 import os
 
+
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return filename
+
+
+def get_final_filename(d):
+    
+    return d['filename']
+
+
+def download_soundfile(self):
+
+    song_url=self.soundfile_url
+    uuid_filename = uuid.uuid4()
+
+    ydl_opts = {
+        'outtmpl': '/website/files/media/' + str(uuid_filename) + '.%(ext)s',
+        'format': 'bestaudio',
+        'postproccesors': [{
+            'key': 'FFmpegExtractAudio',
+             'preferredquality': '1411',
+        }],
+        'progress_hooks': [get_final_filename],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([song_url])
+    return 
+
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist)
@@ -23,3 +50,32 @@ class Song(models.Model):
 
     def __unicode__(self):
         return self.artist.name + " - " + self.name
+
+    def download_soundfile(self):
+
+        def get_final_filename(d):
+            global self.soundfile
+            self.soundfile = d['filename']
+
+        song_url=self.soundfile_url
+        uuid_filename = uuid.uuid4()
+
+        ydl_opts = {
+            'outtmpl': '/website/files/media/' + str(uuid_filename) + '.%(ext)s',
+            'format': 'bestaudio',
+            'postproccesors': [{
+                'key': 'FFmpegExtractAudio',
+                 'preferredquality': '1411',
+            }],
+            'progress_hooks': [get_final_filename],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([song_url])
+  
+    def save(self, *args, **kwargs):
+        try:
+            song = Song.objects.get(pk=self.pk)
+        except: 
+        if self.soundfile_url != song.soundfile_url
+            self.download_soundfile()
