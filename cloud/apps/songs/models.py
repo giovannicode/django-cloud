bbf09e961d2a9dc20fe4db5b39fa96fc164fe39a
Giovanni Arroyo <giovannicode7@gmail.com>
2015-12-09 02:22:54 +0000

    Fix issues with downloading songs by url

--- cloud/apps/songs/models.py
+++ cloud/apps/songs/models.py
@@ -1,50 +1,54 @@
+import uuid
+
+import youtube_dl
+
 from django.contrib.gis.db import models
+from django.conf import settings
+
 
 from apps.artists.models import Artist 
 from apps.genres.models import Genre
 from apps.albulms.models import Albulm
 
-import uuid
-import os
 
-
-def get_file_path(instance, filename):
-    ext = filename.split('.')[-1]
-    filename = "%s.%s" % (uuid.uuid4(), ext)
-    return filename
-
-
-def get_final_filename(d):
-    
-    return d['filename']
-
-
-def download_soundfile(self):
-
-    song_url=self.soundfile_url
-    uuid_filename = uuid.uuid4()
-
-    ydl_opts = {
-        'outtmpl': '/website/files/media/' + str(uuid_filename) + '.%(ext)s',
-        'format': 'bestaudio',
-        'postproccesors': [{
-            'key': 'FFmpegExtractAudio',
-             'preferredquality': '1411',
-        }],
-        'progress_hooks': [get_final_filename],
-    }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([song_url])
-    return 
+#def get_file_path(instance, filename):
+#    ext = filename.split('.')[-1]
+#    filename = "%s.%s" % (uuid.uuid4(), ext)
+#    return filename
+#
+#
+#def get_final_filename(d):
+#    
+#    return d['filename']
+#
+#
+#def download_soundfile(self):
+#
+#    song_url=self.soundfile_url
+#    uuid_filename = uuid.uuid4()
+#
+#    ydl_opts = {
+#        'outtmpl': '/website/files/media/' + str(uuid_filename) + '.%(ext)s',
+#        'format': 'bestaudio',
+#        'postproccesors': [{
+#            'key': 'FFmpegExtractAudio',
+#             'preferredquality': '1411',
+#        }],
+#        'progress_hooks': [get_final_filename],
+#    }
+#
+#    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#        ydl.download([song_url])
+#    return 
 
 
 class Song(models.Model):
     artist = models.ForeignKey(Artist)
-    albulm = models.ForeignKey(Albulm)
+    albulm = models.ForeignKey(Albulm, null=True, blank=True)
     track_number = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=60, blank=False)
-    soundfile = models.FileField(upload_to=get_file_path)
+    soundfile = models.FileField(blank=True)
+    soundfile_url = models.URLField()
     lyrics = models.TextField(null=True, blank=True)
     genre = models.ForeignKey(Genre)
 
@@ -52,10 +56,12 @@ class Song(models.Model):
         return self.artist.name + " - " + self.name
 
     def download_soundfile(self):
+        soundfile = self.soundfile        
 
-        def get_final_filename(d):
-            global self.soundfile
-            self.soundfile = d['filename']
+        def add_file_to_object(d):
+            global soundfile
+            filename = d['filename'].replace(settings.MEDIA_ROOT, '')
+            self.soundfile = filename
 
         song_url=self.soundfile_url
         uuid_filename = uuid.uuid4()
@@ -67,7 +73,7 @@ class Song(models.Model):
                 'key': 'FFmpegExtractAudio',
                  'preferredquality': '1411',
             }],
-            'progress_hooks': [get_final_filename],
+            'progress_hooks': [add_file_to_object],
         }
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
@@ -76,6 +82,8 @@ class Song(models.Model):
     def save(self, *args, **kwargs):
         try:
             song = Song.objects.get(pk=self.pk)
+            if self.soundfile_url != song.soundfile_url:
+                self.download_soundfile()
         except: 
-        if self.soundfile_url != song.soundfile_url
             self.download_soundfile()
+        super(Order, self).save(*args, **kwargs)
