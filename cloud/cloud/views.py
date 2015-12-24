da77cdc970a0b7a31f02b13b9b94c35724dfb381
Giovanni Arroyo <giovannicode7@gmail.com>
2015-12-24 02:08:08 +0000

    Add ordering to artists list on homepage

--- cloud/cloud/views.py
+++ cloud/cloud/views.py
@@ -9,6 +9,6 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['genres'] = Genre.objects.all()
-        context['artists'] = Artist.objects.all()
+        context['artists'] = Artist.objects.order_by('name')
         return context
 
